8eed9db803ad6318fa43e72daa0e33fcea4f71e1
HHKG <34594823+HHKG@users.noreply.github.com>
2017-12-28 22:32:42 +0800

    JS

---
+++ H4.js
@@ -0,0 +1,8 @@
+/*<button onclick = "displayDtae()">点击这里</button>
+<script>
+document.getElementById("myBtn").onclick=function(){displayDate()};
+</script>*/
+//<body onload="checkCookies()">
+<input type="text" id="fname" onchange="upperCase()">
+
+
